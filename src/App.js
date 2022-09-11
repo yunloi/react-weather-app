925c86aaf9d8e8c74b426537629249eb010cbfb1
yunloi <103883589+yunloi@users.noreply.github.com>
2022-09-11 21:03:52 +1000

    updated css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import "./App.css";
+import React from "react";
 import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <Weather />
+
         <footer>
           This project was coded by Yun Loi Lok and is{" "}
           <a
             href="https://github.com/yunloi/react-weather-app"
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
           >
             open sourced on GitHub
           </a>
--- src/Weather.js
+++ src/Weather.js
@@ -6,10 +6,11 @@ export default function Weather() {
     <div className="Weather">
       <form>
         <div className="row">
-          <div className="col-9">
+          <div className="col-6">
             <input
               type="search"
               placeholder="Enter a city..."
+              autoFocus="on"
               className="form-control"
             />
           </div>
@@ -21,22 +22,30 @@ export default function Weather() {
       <h1>Sydney</h1>
       <h2>Saturday, 10 September 16:00</h2>
       <div className="row">
-        <div className="col-3">
-          <span>icon image</span>
+        <div className="col-4 mt-2">
+          <div className="clearfix">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/1779/1779940.png"
+              alt="icon"
+            />
+          </div>
         </div>
         <div className="col-3">
           <ul>
-            <li>Weather Description</li>
-            <li>15℃</li>
+            <li>Description</li>
+            <li>
+              <span className="temperature">15</span>
+              <span className="unit">℃</span>
+            </li>
           </ul>
           <h3>Now</h3>
         </div>
-        <div className="col-6">
+        <div className="col-4">
           <ul>
-            <li>Max Temperature</li>
-            <li>Min Temperature</li>
-            <li>Wind</li>
-            <li>Humidity</li>
+            <li>High: ℃</li>
+            <li>Low: ℃</li>
+            <li>Wind: m/s</li>
+            <li>Humidity: %</li>
           </ul>
         </div>
       </div>
