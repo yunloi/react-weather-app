2d782d2d6eee985551a01ccce24d9cdff3730de6
yunloi <103883589+yunloi@users.noreply.github.com>
2022-09-12 22:55:42 +1000

    updated unit conversion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,8 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
+import "./Weather.css";
 
 export default function WeatherInfo(props) {
   return (
@@ -10,24 +12,21 @@ export default function WeatherInfo(props) {
         <FormattedDate date={props.data.date} />
       </h2>
       <div className="row mt-4">
-        <div className="col-4">
+        <div className="col-3">
           <div className="d-flex justify-content-center">
             <WeatherIcon code={props.data.icon} />
           </div>
         </div>
-        <div className="col-3">
+        <div className="col-4">
           <ul>
             <li className="text-capitalize">{props.data.description}</li>
             <li>
-              <span className="temperature">
-                {Math.round(props.data.temperature)}
-              </span>
-              <span className="unit">℃</span>
+              <WeatherTemperature celsius={props.data.temperature} />
             </li>
             <h3>Now</h3>
           </ul>
         </div>
-        <div className="col-4">
+        <div className="col-3">
           <ul>
             <li>High: {Math.round(props.data.maxTemperature)}℃</li>
             <li>Low: {Math.round(props.data.minTemperature)}℃</li>
